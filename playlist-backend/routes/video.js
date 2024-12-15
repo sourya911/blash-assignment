44d916bfa7d5c39c86d35ecfd730667bc7426973
sourabh <magdumsourabh37@gmail.com>
2024-12-15 13:26:43 +0530

    modified auth file

--- playlist-backend/routes/video.js
+++ playlist-backend/routes/video.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { verifyToken }=require("../middleware/authMiddleware")
+
 const { addVideo, getUserVideos,getUserWithVideos } = require('../controllers/videoController'); 
 
 
